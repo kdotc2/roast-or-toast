c8318c6c7c9c8edb7f25dbccb55eeba1b9ce89cc
JSON <jasonsalter2@gmail.com>
2023-04-18 23:42:57 -0700

    updates meta tags (#8)
    Co-authored-by: Jason Salter <json@localgames.online>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
 import PostFeed from '@/components/Posts/PostFeed'
-import { PageSEO } from '@/data/SEO'
-import siteMetadata from '@/data/siteMetadata'
 import React from 'react'
 
 export default function Home() {
   return (
     <>
-      <PageSEO
-        title={siteMetadata.title}
-        description={siteMetadata.description}
-      />
       <PostFeed />
     </>
   )
--- src/pages/post/[pid].tsx
+++ src/pages/post/[pid].tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import usePosts from '@/hooks/usePosts'
 import { Post } from '@/atoms/postAtom'
+import Meta from '@/components/Meta'
 
 const PostPage = () => {
   const [post, setPost] = useState<Post | undefined>(undefined)
@@ -29,9 +30,15 @@ const PostPage = () => {
 
   return (
     <>
+    <Meta
+      title={post?.title}
+      desc={post?.body}
+      imageURL={post?.imageURL}
+      url={post?.url}
+    />
       <div>
         <div className="fixed inset-0 z-[90] flex items-center justify-center">
-          <PostDetail  post={post} /> 
+          <PostDetail post={post} /> 
         </div>
       </div>
     </>
