56896f86cc05b3e45e10104ba7619a2f6531e1d8
Kent <kentsitu@icloud.com>
2025-04-30 23:16:27 -0700

    fix sign in with email

--- src/components/Modal/Auth/SignInWithEmail.tsx
+++ src/components/Modal/Auth/SignInWithEmail.tsx
@@ -22,7 +22,7 @@ type Props = {
 const getEmailLink = () => {
   return process.env.NEXT_PUBLIC_LOCAL_DEV
     ? 'http://localhost:3000'
-    : 'https://rot-chi.vercel.app'
+    : 'https://rotapp-f5483.web.app'
 }
 
 export const SignInWithEmail = ({ close }: Props) => {
@@ -91,14 +91,6 @@ export const SignInWithEmail = ({ close }: Props) => {
     sendSignInLinkToEmail(auth, email, {
       url: getEmailLink(),
       handleCodeInApp: true,
-      iOS: {
-        bundleId: 'com.example.ios',
-      },
-      android: {
-        packageName: 'com.example.android',
-        installApp: true,
-        minimumVersion: '12',
-      },
     })
       .then((result) => {
         localStorage.setItem('email', email)
